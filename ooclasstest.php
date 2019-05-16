ecf7cd6587cc230f62c2d5b288041a2ebd77fc21
dipeshrai123 <raidipesh78@gmail.com>
2019-05-16 18:27:04 +0545

    ooclasstest

--- ooclasstest.php
+++ ooclasstest.php
@@ -19,6 +19,66 @@
     
     <div class="container mt-5">
         <!-- SECTIONS -->
+
+        <h1>
+            Classes and Objects
+        </h1>
+
+        <h6>Object Oriented Mysqli and Basic Selection</h6>
+
+        <?php 
+            // Object Oriented Mysqli
+            $con = new mysqli('localhost', 'root', '', 'fashion_shop');
+
+            if($con->connect_error) {
+                die("Connection Error!");
+            }
+
+            // Basic Selection
+            $sql = "SELECT * FROM tbl_items";
+            $result = $con->query($sql);
+
+            if($result->num_rows > 0) {
+                while($row = $result->fetch_assoc()) {
+                    $item_id = $row['item_id'];
+                    $item_name = $row['item_name'];
+                    $item_quantity = $row['item_quantity'];
+
+                    ?>
+                        <div>
+                            <?php 
+                                echo $item_id . " - " . $item_name . " - " . $item_quantity;
+                            ?>
+                        </div>
+                    <?php
+                }
+            }
+
+        ?>
+
+        <h6 style="margin-top: 40px;">Object Oriented Prepared Statements</h6>
+
+        <?php 
+            // PREPARED STATEMENT
+            $stmt = $con->prepare("SELECT * FROM tbl_items WHERE item_id = ?");
+            $stmt->bind_param("i", $id);
+
+            $id = 5;
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+
+            $row = $result->fetch_assoc();
+            echo $row['item_id'] . " - " . $row['item_name'] . " - " . $row['item_quantity'] . "<br>";
+            
+            $id = 6;
+            $stmt->execute();
+
+            $result = $stmt->get_result();
+
+            $row = $result->fetch_assoc();
+            echo $row['item_id'] . " - " . $row['item_name'] . " - " . $row['item_quantity'];
+        ?>
     </div>
 </body>
 </html>
