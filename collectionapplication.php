9912ffd9ad1b858f8d986edd3381b585844bd6f4
dipeshrai123 <raidipesh78@gmail.com>
2019-05-16 18:39:46 +0545

    collectionapplication

--- collectionapplication.php
+++ collectionapplication.php
@@ -29,6 +29,13 @@
         <h1>Collection : Application</h1>
 
         <div id="display_items">
+            <h5>Items from the database:</h5>
+
+            <div class="collection_each">
+                <div class="collection_each_name">ITEM NAME</div>
+                <div class="collection_each_quantity">QUANTITY</div>
+            </div>
+
             <div class="collection_each">
                 <div class="collection_each_name">Dipesh Rai</div>
                 <div class="collection_each_quantity">10</div>
